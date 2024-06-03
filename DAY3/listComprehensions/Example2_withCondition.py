ce816ff56c06fb72ba19437f4e2fcc7450928ea0
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-06-03 13:46:52 +0545

    Learnt about List comprehension

---
+++ DAY3/listComprehensions/Example2_withCondition.py
@@ -0,0 +1,2 @@
+squares = [x**2 for x in range(10) if x % 2 == 0] #returns the squares of even numbers within the range of 10
+print(squares)
---
+++ DAY3/listComprehensions/listComprehension_Example1.py
@@ -0,0 +1,5 @@
+'''
+List Comprehensions provide a syntactic way of generating lists from sequences like lists, tuples and strings
+'''
+squares = [x**2 for x in range(10)]
+print(squares)
