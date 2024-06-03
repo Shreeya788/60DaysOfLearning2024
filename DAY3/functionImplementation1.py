474c39edffb18f1c867317972fcb5b00ea12fb59
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-06-03 13:27:05 +0545

    Refactor: code into a  folder

--- DAY3/functionImplementation1.py
+++
@@ -1,5 +0,0 @@
-def calculate_tax(bill,tax_rate):
-    total_tax = (bill*tax_rate) /100;
-    return total_tax
-print('Total Tax is : ',calculate_tax(175.00,15)) 
-print('Total Tax is : ',calculate_tax(164.33,22)) 
--- DAY3/python_functions.py
+++
@@ -1,7 +0,0 @@
-'''
-A function is a block of code which only runs when it is called. Data are passed into function as parameters.
-'''
-def my_function():
-    print("Hello from a function")
-
-my_function()
