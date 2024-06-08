41f570903ed7296c3bae31eb401bb73e68eb9736
Shreeya Shrestha <shresthashreeya788@gmail.com>
2024-06-08 21:11:23 +0545

    Learnt about Map Function

---
+++ DAY7/mapFunction.py
@@ -0,0 +1,8 @@
+coffees = ['Espresso','Latte','Cappuccino','Machhiato','Americano','Decaf']
+
+def reverse_str(str):
+    return str[::-1]
+reversed_coffees = map(reverse_str,coffees)
+for x in reversed_coffees:
+    print(x)
+
